c443ba9310b0ca7c43091e8b134386946f259a49
Timothée ROUSSELLE <t.rousselle@symaris.com>
2017-04-17 15:16:09 +0200

    Ajout du template de base pour mes scripts

---
+++ 0_Template/interface_sans_interaction.ps1
@@ -0,0 +1,43 @@
+
+#Requires -Version 3.0
+
+<#
+.SYNOPSIS 
+Bref description du script
+.DESCRIPTION 
+Description intégrale du script
+.PARAMETER name 
+Description des paramètres (un parametre / .)
+.EXAMPLE 
+Exemple d'usage
+.NOTES
+Auteur:
+Version:
+.LINK 
+https://github.com/MrZ3ki3l/PowerShell
+#>
+
+
+# Message de presentation l'utilisateur
+Write-Host ""
+Write-Host "=================================================================="
+Write-host "=    Ce script     ="
+Write-Host "=								 ="
+Write-Host "=								 ="
+Write-Host "= Version:		         Réalisation:  ="
+Write-Host "=================================================================="
+Write-Host ""
+
+# Interface
+Write-Host ""
+Write-Host "======================================"
+Write-Host "=	Lancement du script	     ="
+Write-Host "======================================"
+Write-Host ""
+
+# Traitement
+
+# Message de au revoir
+Write-Host ""
+Write-Host ""
+Write-Host "Au revoir !"
